44dc1988af45a64e55cada2aafc7265d31683efa
munazzaz <munazzazahid5@gmail.com>
2025-05-13 09:44:18 -0700

    fixed font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,32 +13,9 @@ const geistMono = Geist_Mono({
   
 });
 
-const siteUrl = "https://microsite-lime.vercel.app";
-
-
 export const metadata: Metadata = {
   title: "Beiz",
   description: "Pre-Register Your Spot",
-   openGraph: {
-    title: "Beiz | Pre-Register",
-    description: "Join Beiz today and secure your early access",
-    url: siteUrl,
-    images: [
-      {
-        url: `${siteUrl}/logo-og.png`,
-        width: 1200,
-        height: 630,
-        alt: "Beiz Logo",
-      },
-    ],
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Beiz | Pre-Register",
-    description: "Secure your spot on Beiz—the future of networking is here",
-    images: [`${siteUrl}/logo-og.png`],
-  },
 };
 
 export default function RootLayout({
